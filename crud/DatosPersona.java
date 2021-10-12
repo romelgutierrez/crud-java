e214c48b703ffa907dd4ec0256a5fe3da77ca4e9
romelgutierrez <romelgutierrez90@gmail.com>
2021-10-11 22:23:22 -0500

    crud java interfaz

---
+++ crud/DatosPersona.java
@@ -0,0 +1,10 @@
+package crudvector01;
+
+public class DatosPersona {
+    
+    public String nombres;
+    public String ap_paterno;
+    public String ap_materno;
+    public String codigo;
+    
+}
